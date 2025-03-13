c521e9f2a808b83bbe321eaa8dd3b3fb12e500aa
Subhankito Roy Choudhury <subhankitoroychoudhuryofficial@gmail.com>
2025-03-13 13:49:11 +0530

    UPDATED

--- script.js
+++ script.js
@@ -5,17 +5,36 @@ function changeCircleColor(newColor) {
     const circle = document.querySelector('.circle');
     circle.style.background = newColor;
   }
-  document.getElementById("instagram").addEventListener("click", () => {
-    console.log("Instagram clicked");
-    window.location.href = "https://www.instagram.com/starbucks/?hl=en";
+  document.querySelectorAll(".instagram").forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("Instagram clicked");
+        window.location.href = "https://www.instagram.com/starbucks/?hl=en";
+    });
 });
 
-document.getElementById("facebook").addEventListener("click", () => {
-    console.log("Facebook clicked");
-    window.location.href = "https://www.facebook.com/Starbucks/";
+document.querySelectorAll(".facebook").forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("Facebook clicked");
+        window.location.href = "https://www.facebook.com/Starbucks/";
+    });
 });
 
-document.getElementById("twitter").addEventListener("click", () => {
-    console.log("Twitter clicked");
-    window.location.href = "https://twitter.com/StarbucksIndia";
+document.querySelectorAll(".twitter").forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("Twitter clicked");
+        window.location.href = "https://twitter.com/StarbucksIndia";
+    });
+});
+document.querySelector(".learn-btn").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+});
+
+document.querySelector(".close-btn").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "none";
+});
+
+document.getElementById("modal").addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal")) { 
+        document.getElementById("modal").style.display = "none";
+    }
 });
